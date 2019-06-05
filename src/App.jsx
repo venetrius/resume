93cf689f75780c19e208372acc9651a15a09bd3e
venetrius <gergelygjuhasz@gmail.com>
2019-06-05 10:07:55 -0600

    add skills

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Experience from './Components/Experience.jsx';
+import Skill from './Components/Skill.jsx'
 import data from './dummydata.js'
 
 class App extends Component {
@@ -14,7 +15,7 @@ class App extends Component {
         </div>
 
         <p>
-          <h2 className="center">Key Strengths</h2>
+          <h2 className="center">Summary</h2>
           <ul>
             <li>Core proficiencies include JavaScript, Java 8 and SQL</li>
             <li>A growth mindset enjoys delving into highly technical problems</li>
@@ -23,29 +24,21 @@ class App extends Component {
 
           </ul>
         </p>
+
+        <h2> Skills</h2>
+        {data.skills.map(skill => <Skill data={skill} />)}
+
         <p>
           <h2 className="center"> Relevant Experience</h2>
           {data.workExperience.map(item => <Experience  experience={item} />)}  
-        </p>
-        <p>
-          <h2 class="center"> Other Experience</h2>
-          <div class="experience">
-            <h3 class="title"> Volunteer<span class="dateInterval"> Sept. 2012 - Dec. 2012</span></h3>
-            <h4>Solidair met India, Ghosi, Uttar Prades, India</h4>
-
-            <ul>
-              <li>Taught Science, Math and Computer Science at St. Norbert School
-            for classes with 30-40 students of ages 8 - 16</li>
-            </ul>
-          </div>
         </p>
 
         <p>
           <h2 class="center"> Education</h2>
           {data.education.map(item => <Experience  experience={item} />)}        
-    </p>
-</div>
-                  );
-                }
+        </p>
+      </div>
+    );
+  }
 }
 export default App;
---
+++ src/Components/Skill.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+
+ function Skill (props)  {
+  return <span className="skill">  {' ' + props.data + ' '}  </span>
+};
+export default Skill;
